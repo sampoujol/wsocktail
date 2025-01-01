9da103639427d15a922b38f54d9104900373fee8
Samuel Poujol <sampoujol@free.fr>
2025-01-01 18:49:27 +0100

    Unification du code, nettoyage de la logique et commentaires

--- wsocktail.js
+++ wsocktail.js
@@ -4,69 +4,83 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as readline from 'readline';
 
+
+/**
+ * Lit et envoie  sur le websocket len octets depuis la position startpos du fichier
+ * @param ws la websocket ouverte
+ * @param options
+ * @param startpos la position de debut de lecture
+ * @param len la longueur à lire
+ */
+function readNSend(ws, options, startpos, len){
+  console.log("entering readNSend");
+  // creation d'un tampon pour lire les nouveautés
+  var buffer = new Buffer(len);
+  var fileDescriptor = fs.openSync(options.logFile, 'r');
+  // lecture des nouveautés
+  fs.readSync(fileDescriptor, buffer, 0, len, startpos);
+  fs.closeSync(fileDescriptor); // close the file
+  console.log("sending");
+  wsSendBuf(ws, options, buffer);
+}
+
+
+/**
+ * Envoie ligne par ligne le contenu de buffer sur le websocket ws
+ * @param ws
+ * @param options
+ * @param buffer
+ */
+function wsSendBuf (ws , options, buffer) {
+  console.log("entering wsSendBuf");
+  // Iterate over each line in the buffer.
+  buffer.toString().split(options.endOfLineChar).forEach(function (line) {
+    // Do stuff with the line :)
+    ws.send(line);
+  });
+}
+
+
+/**
+ * Callback executé à lors de la connection d'un client au websocket
+ * @param ws
+ */
 function connect(ws){
+
+  // structure de donnees decrivant le fichier à scruter
   var options = {
     logFile: 'toto.txt',
     endOfLineChar: os.EOL
   };
-  
 
-  //on envoie le debut du fichier
-  const inputStream = fs.createReadStream(options.logFile);
-  var lineReader = readline.createInterface({
-    input: inputStream,
-    terminal: false,
-  });
-  lineReader.on("line", function (line) {
-    ws.send(line);
-  });
+  //etape 1, on lit le fichier en entier avant d'attendre les changements
+  console.log("Reading first part");
+  var spos = 0;
+  var fsz = fs.statSync(options.logFile).size;
+  readNSend(ws, options, spos,fsz);
+  spos=fsz;
 
-
-
-
-  // Obtain the initial size of the log file before we begin watching it.
-  var fileSize = fs.statSync(options.logFile).size;
+  //etape 2, on scrute le fichier pour observer d'éventuels changements
+  console.log("now waiting for changes");
   fs.watchFile(options.logFile, function (current, previous) {
-    // Check if file modified time is less than last time.
-    // If so, nothing changed so don't bother parsing.
-    if (current.mtime <= previous.mtime) { return; }
-
-    // We're only going to read the portion of the file that
-    // we have not read so far. Obtain new file size.
-    var newFileSize = fs.statSync(options.logFile).size;
-    // Calculate size difference.
-    var sizeDiff = newFileSize - fileSize;
-    // If less than zero then Hearthstone truncated its log file
-    // since we last read it in order to save space.
-    // Set fileSize to zero and set the size difference to the current
-    // size of the file.
-    if (sizeDiff < 0) {
-      fileSize = 0;
-      sizeDiff = newFileSize;
+    console.log("Just woke up");
+    // On envoie les dernieres lignes du fichier
+    // - si la date de dèrniere modification a changé
+    if (current.mtime > previous.mtime) {
+      fsz = fs.statSync(options.logFile).size;
+      var sizeDiff = fsz - spos;
+      /* si la difference  de taille est negative le fichié a été tronqué */
+      if (sizeDiff < 0) {
+        spos = 0;
+        sizeDiff = newFileSize;
+      }
+      readNSend(ws, options,spos,sizeDiff);
+      spos=fsz;
     }
-    // Create a buffer to hold only the data we intend to read.
-    var buffer = new Buffer(sizeDiff);
-    // Obtain reference to the file's descriptor.
-    var fileDescriptor = fs.openSync(options.logFile, 'r');
-    // Synchronously read from the file starting from where we read
-    // to last time and store data in our buffer.
-    fs.readSync(fileDescriptor, buffer, 0, sizeDiff, fileSize);
-    fs.closeSync(fileDescriptor); // close the file
-    // Set old file size to the new size for next read.
-    fileSize = newFileSize;
-
-    // Parse the line(s) in the buffer.
-    parseBuffer(buffer);
   });
 
-  function parseBuffer (buffer) {
-    // Iterate over each line in the buffer.
-    buffer.toString().split(options.endOfLineChar).forEach(function (line) {
-      // Do stuff with the line :)
-       ws.send(line);
-    });
-  }
 
+  //on libere les ressources proprement lors de la deconnexion
   ws.on('close',function bye(){
     fs.unwatchFile(options.logFile);
   });
