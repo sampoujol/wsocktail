9cd7218f191a41e852b9ad4425d1947002bb39cf
Duran Duran <sampoujol@free.fr>
2025-01-08 10:55:54 +0100

    client added

---
+++ static/js/tailclient.js
@@ -0,0 +1,35 @@
+
+
+function connect(){
+    var zone=document.getElementsByClassName("termwin")[0];
+    var socket = new WebSocket("ws://localhost:8087");
+    socket.onopen=function (event){
+        established(event,zone);
+    }
+    socket.onmessage=function(event){
+        newdata(event,zone);
+    };
+    socket.onclose=function(event){
+        srvdiconnect(event,zone);
+    }
+    socket.onerror=function(event){
+        error(event,zone)
+    };
+
+}
+
+function established(event,zone){
+    zone.innerText+="Connexion établie";
+}
+
+function newdata(event,zone){
+    zone.innerText+=event.data;
+}
+
+function srvdiconnect(event,zone){
+    zone.innerText+="Disconnected";
+}
+
+function error(event,zone){
+    zone.innerText+="Erreur";
+}
